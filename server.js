7a7381093dad5452c1829a28ec7feb6989c5d586
Marcus Ang <ang.marcus.incosys@gmail.com>
2014-10-03 18:02:05 +0800

    First Commit

---
+++ server.js
@@ -0,0 +1,65 @@
+﻿var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+
+var cache = {};
+
+//Send 404 message
+function send404(response) {
+    response.writeHead(404, { 'Content-Type': 'text/plain' });
+    response.write('Error 404: resource not found');
+    response.end();
+}
+
+//Serve file data
+function sendFile(response, filePath, fileContents) {
+    response.writeHead(
+            200,
+            {"content-type": mime.lookup(path.basename(filePath))}
+        );
+    response.end(fileContents);
+}
+
+//Serve static files
+function serveStatic(response, cache, absPath) {
+    if (cache[absPath]) {
+        sendFile(response, absPath, cache[absPath]);
+    } else {
+        fs.exists(absPath, function (exists) {
+            if (exists) {
+                fs.readFile(absPath, function (err, data) {
+                    if (err) {
+                        send404(response);
+                    } else {
+                        cache[absPath] = data;
+                        sendFile(response, absPath, data);
+                    }
+                });
+            } else {
+                send404(response);
+            }
+        });
+    }
+}
+
+//Create an HTTP Server
+var server = http.createServer(function (request, response) {
+    var filePath = false;
+
+    if (request.url == '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public' + request.url;
+    }
+
+    var absPath = './' + filePath;
+    serveStatic(response, cache, absPath);
+});
+
+server.listen(3000, function () {
+    console.log("Server listening on port 3000.");
+});
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
